bef1e7b760b5008421d841dc21cbc62a33b1c8b5
Jamaica <166144244+Jam-jr18@users.noreply.github.com>
2026-05-26 13:40:02 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
-});
+  plugins: [
+    react(),
+    viteSingleFile()
+  ],
+
+  server: {
+    host: '0.0.0.0'
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT,
+    allowedHosts: [
+      'fabon-jamaica-rose-final-exam.onrender.com'
+    ]
+  }
+})
